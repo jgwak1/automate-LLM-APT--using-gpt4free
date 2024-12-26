91e1ffe25693f65fff8ea2e9dab174e3a0cab6bb
Joon-Young Gwak <jgwak1@binghamton.edu>
2024-12-25 21:12:14 -0500

    based on further updated subtask 1 prompt 5

--- Automate_Subtask_1.py
+++ Automate_Subtask_1.py
@@ -37,19 +37,93 @@ def get__subtask_1_prompts_list( code_snippet : str = "", background_info : str 
 
     prompt_5 = "Step 4: Adjust the level of detail in the description from Step 3 to match the level typically found in a description of a MITRE ATT&CK technique. Please do not tag the final description with any MITRE ATT&CK Tactics, Techniques, or Procedures (TTPs)"
 
-    # prompt_5_Modified = (
+    # prompt_5_First_Modified = (
     #     f"Step 4: Adjust the description to focus more on the higher-level intention of the code (similar to a typical description of a MITRE ATT&CK technique) rather than being providing plain language explanations of each line of code."
     #     f"Please do not tag the final description with any MITRE ATT&CK Tactics, Techniques, or Procedures (TTPs)"
     # ) 
-    prompt_5_Modified = (
-        "Step 4: Refine the description from Step 3 to emphasize the higher-level intention "
-        "or overarching purpose of the code, using language similar to a typical MITRE ATT&CK technique description. "
-        "This should highlight the strategic objective rather than detailing individual lines of code or actions. "
 
+    # prompt_5_Second_Modified = (
+    #     "Step 4: Refine the description from Step 3 to emphasize the higher-level intention "
+    #     "or overarching purpose of the code, using language similar to a typical MITRE ATT&CK technique description. "
+    #     "This should highlight the strategic objective rather than detailing individual lines of code or actions. "
+
+    #     "Please do not tag the final description with any MITRE ATT&CK Tactics, Techniques, or Procedures (TTPs)."
+    # )
+
+    return [ prompt_1, prompt_2, prompt_3, prompt_4, prompt_5 ]
+
+
+
+
+def get__subtask_1_prompts_list__POST_background( code_snippet : str = "", background_info : str = "N/A" ):
+
+    prompt_1 = (
+            # f"Given the code snippet and its background information (if available) below, "
+            f"Given the code snippet below, "
+            f"provide a concise behavioral description based on the following 4 steps. "
+            f"I will provide you the 4 steps sequentially, so do not start yet:\n\n"
+            f"Code snippet:\n{code_snippet}\n\n"
+            # f"Background information (if available):\n{background_info}\n\n"
+            # f"Please do not tag the final description with any MITRE ATT&CK Tactics, Techniques, or Procedures (TTPs)."
+    )
+
+    # prompt_2 = "Step 1: Examine the code snippet and its background information (if available) to identify its core function, such as making API calls, performing file manipulations, interacting with system-level resources, or initiating network communications. Pay close attention to any options or flags that specify how the command operates."
+
+    prompt_2 = "Step 1: Examine the code snippet to identify its core function, such as making API calls, performing file manipulations, interacting with system-level resources, or initiating network communications. Pay close attention to any options or flags that specify how the command operates."
+    prompt_3 = "Step 2: Identify the inputs and outputs, including the data or parameters the commands accept (e.g., task name, file path, user or group name), and the resulting actions or outcomes they produce (e.g., creating a scheduled task, listing group members, or displaying output)."
+    prompt_4 = "Step 3: Describe the actions performed by the commands in plain language, focusing on what it does and its potential impact on the system or environment."
+
+    prompt_btwn_4n5 = (
+            f"Step 4: Following is the background information for the code snippet, which includes information relevant to the code snippet's behavior but not directly available from the code snippet itself "
+            f"(e.g., the code snippet shows only the function usage, while the background information provides the implementation of that function). "
+            f"Adjust the description from Step 3 to reflect the complete behavior of the code snippet, supplemented by the provided background information.\n\n"
+            f"Background information:\n{background_info}\n\n"
+    )
+
+    # prompt_5 = (
+    #     "Step 5: Produce a final plain-language description by refining the description from Step 4 to capture not only the key actions, key features and potential impact of the code snippet but also its higher-level objective, similar to a typical MITRE ATT&CK technique description. "
+    #     "Please do not tag the final description with any MITRE ATT&CK Tactics, Techniques, or Procedures (TTPs)."
+    # )
+
+    prompt_5_v2 = (
+        "Step 5: Produce a final plain-language description by refining the description from Step 4 to focus on the key actions, key features, potential impact, and higher-level objective of the code snippet in a clear and concise manner, avoiding excessive details. "
+        "Do not include any tags for MITRE ATT&CK Tactics, Techniques, or Procedures (TTPs)."
+    )
+
+
+    return [ prompt_1, prompt_2, prompt_3, prompt_4, prompt_btwn_4n5, prompt_5_v2 ]
+
+
+def get__subtask_1_prompts_list__PRE_background( code_snippet : str = "", background_info : str = "N/A" ):
+
+    prompt_1 = (
+            # f"Given the code snippet and its background information (if available) below, "
+            f"Given the code snippet below, "
+            f"provide a concise behavioral description based on the following 4 steps. "
+            f"I will provide you the 4 steps sequentially, so do not start yet:\n\n"
+            f"Code snippet:\n{code_snippet}\n\n"
+            # f"Background information (if available):\n{background_info}\n\n"
+            f"Please do not tag the final description with any MITRE ATT&CK Tactics, Techniques, or Procedures (TTPs)."
+    )
+
+    prompt_2 = "Step 1: Examine the code snippet and its background information (if available) to identify its core function, such as making API calls, performing file manipulations, interacting with system-level resources, or initiating network communications. Pay close attention to any options or flags that specify how the command operates."
+    prompt_3 = "Step 2: Identify the inputs and outputs, including the data or parameters the commands accept (e.g., task name, file path, user or group name), and the resulting actions or outcomes they produce (e.g., creating a scheduled task, listing group members, or displaying output)."
+    prompt_4 = "Step 3: Describe the actions performed by the commands in plain language, focusing on what it does and its potential impact on the system or environment."
+
+    prompt_5 = (
+        "Step 5: Adjust the description from Step 4 using language similar to a typical MITRE ATT&CK technique description. "
         "Please do not tag the final description with any MITRE ATT&CK Tactics, Techniques, or Procedures (TTPs)."
     )
 
-    return [ prompt_1, prompt_2, prompt_3, prompt_4, prompt_5_Modified ]
+
+
+
+    return [ prompt_1, prompt_2, prompt_3, prompt_4, prompt_5 ]
+
+
+
+
+
 
 
 def g4f_generate( prompt_list : list ):
@@ -107,7 +181,11 @@ if __name__ == "__main__":
 
 
 #    outputs_dir_path = "/home/jgwak1/gpt4free_JY/subtask_1_outputs__With_Modified_Prompt_5__2024_12_24"
-   outputs_dir_path = "subtask_1_outputs__With_Second_Modified_Prompt_5__2024_12_24"
+#    outputs_dir_path = "subtask_1_outputs__With_Second_Modified_Prompt_5__2024_12_24"
+
+#    outputs_dir_path = "subtask_1_outputs__POST_background__2024_12_24"
+
+   outputs_dir_path = "subtask_1_outputs__POST_background__2024_12_25__Produce_More_Clear_and_Concise_Final_Description"
 
 #    apt29_collection_t1005_sample_path = "code_samples/externally_dependent_on_scripts/apt29/collection--T1005  Data from Local System--windows"
 
@@ -121,30 +199,34 @@ if __name__ == "__main__":
 
    sample_paths_list = get_sample_paths( "/home/jgwak1/gpt4free_JY/code_samples" )
 
-   samples_to_reexamine = \
-   [ 
-    # [ Not in Top-10 list ]
-   "apt29/collection--T1005  Data from Local System--windows", 
-   "apt29/defensive-evasion--T1134.002  Access Token Manipulation  Create Process with Token--windows", 
-   "apt29/discovery--T1033  System Owner,User Discovery--windows",
-   # "apt29/execution--T1204.002  User Execution  Malicious File--windows", # Discard as background info is too long
-   "apt29/privilege-escalation--T1134.001  Access Token Manipulation  Token Impersonation,Theft--windows",
-   "carbanak/defense-evasion--T1140  Deobfuscate,Decode Files or Information--windows",
-   # "fin6/credential-access--T1003.001  OS Credential Dumping  LSASS Memory - Invoke-Mimikatz--windows"  # Discard as background info is too long
-   "menu_pass/execution--T1047  Windows Management Instrumentation--windows",
+#    samples_to_reexamine = \
+#    [ 
+#     # [ Not in Top-10 list ]
+#    "apt29/collection--T1005  Data from Local System--windows", 
 
-   # [ Top 6 or 7 ]
+#    "apt29/defensive-evasion--T1134.002  Access Token Manipulation  Create Process with Token--windows", 
+#    "apt29/discovery--T1033  System Owner,User Discovery--windows",
 
-   "carbanak/collection--T1005  Data from Local System--linux",
-   "fin6/privilege-escalation--T1134: Access Token Manipulation--windows",
-   "oilrig/Ingress Tool Transfer--T1105  Ingress Tool Transfer--Windows"
-   ] 
+#    # "apt29/execution--T1204.002  User Execution  Malicious File--windows", # Discard as background info is too long
+#    "apt29/privilege-escalation--T1134.001  Access Token Manipulation  Token Impersonation,Theft--windows",
+#    "carbanak/defense-evasion--T1140  Deobfuscate,Decode Files or Information--windows",
+#    # "fin6/credential-access--T1003.001  OS Credential Dumping  LSASS Memory - Invoke-Mimikatz--windows"  # Discard as background info is too long
+#    "menu_pass/execution--T1047  Windows Management Instrumentation--windows",
 
-   sample_paths_list = [ sample_path for sample_path in sample_paths_list if any( [ sample_path.endswith(s) for s in samples_to_reexamine ] ) ] 
+#    # [ Top 6 or 7 ]
 
+#    "carbanak/collection--T1005  Data from Local System--linux",
+#    "fin6/privilege-escalation--T1134: Access Token Manipulation--windows",
+#    "oilrig/Ingress Tool Transfer--T1105  Ingress Tool Transfer--Windows"
+# #    ] 
+
+#    previously_poor_sample_paths_list = [ sample_path for sample_path in sample_paths_list if any( [ sample_path.endswith(s) for s in samples_to_reexamine ] ) ] 
+
+#    sample_paths_list = [ sample_path for sample_path in sample_paths_list if sample_path not in previously_poor_sample_paths_list ] 
 
 
    for sample_path in sample_paths_list:
+        
        
         code_snippet_file_path = os.path.join(sample_path, "code_snippet.txt")
         background_info_file_path = os.path.join(sample_path, "background_info.txt")
@@ -160,7 +242,13 @@ if __name__ == "__main__":
             background_info_str = "N/A"
 
 
-        sample__subtask_1_prompt_list = get__subtask_1_prompts_list( code_snippet= code_snippet_str , background_info= background_info_str )
+        # sample__subtask_1_prompt_list = get__subtask_1_prompts_list( code_snippet= code_snippet_str , background_info= background_info_str )
+
+
+        sample__subtask_1_prompt_list = get__subtask_1_prompts_list__POST_background( code_snippet= code_snippet_str , background_info= background_info_str )
+
+        # get__subtask_1_prompts_list__POST_background
+
 
         sample__prompt_and_response_dict_list = g4f_generate( sample__subtask_1_prompt_list )
 
